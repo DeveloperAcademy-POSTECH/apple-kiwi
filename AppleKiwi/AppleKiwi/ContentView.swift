6b61a91b9e2ef48a2eb702189a0b73a514a418f4
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2022-04-07 16:55:17 +0900

    비밀번호 변경 페이지 연결

--- AppleKiwi/AppleKiwi/ContentView.swift
+++ AppleKiwi/AppleKiwi/ContentView.swift
@@ -15,29 +15,6 @@ struct ContentView: View {
     }
 }
 
-                
-                Spacer()
-            }
-            
-            List {
-                Button("알림설정") {
-                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
-                }
-                Button("비밀번호 변경") {
-                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
-                }
-                Button("기본 방침") {
-                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
-                }
-                Button("회원 탈퇴") {
-                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
-                }
-             
-            }
-                
-            }
-        }
-    }
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()

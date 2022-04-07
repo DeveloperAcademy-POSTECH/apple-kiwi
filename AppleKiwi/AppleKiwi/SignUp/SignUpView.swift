2d0b58e25175f3b988796bf3260068f2cfd4c3bc
MBSoo <qud9783@khu.ac.kr>
2022-04-07 16:42:44 +0900

    SignUp

--- AppleKiwi/AppleKiwi/SignUp/SignUpView.swift
+++ AppleKiwi/AppleKiwi/SignUp/SignUpView.swift
@@ -8,27 +8,73 @@
 import SwiftUI
 
 struct SignUpView: View {
+    @State var email:String = ""
+    @State var verify:String = ""
+    @State private var EnterVerify = false
     var body: some View {
-        NavigationView {
-            NavigationLink(destination: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Destination@*/Text("Destination")/*@END_MENU_TOKEN@*/) {
-                Text("인증번호 전송")
-                    .fontWeight(.bold)
-                    .foregroundColor(Color.white)
-                    .frame(width: 200.0, height: 50.0)
+        NavigationView{
+            VStack {
+                HStack(alignment: .firstTextBaseline){
+                    Text("이메일")
+                        .padding(.leading)
+                    Spacer()
+                    TextField("appledev@pos.idserve.net", text: $email)
+                        .keyboardType(.emailAddress)
+                        .textFieldStyle(.automatic)
+                        .frame(width: 230, height: 40, alignment: .leading)
+                    Spacer()
+                }
+                HStack(alignment: .firstTextBaseline){
+                    if(EnterVerify)
+                    {
+                        Text("인증번호")
+                        .padding(.leading)
+                        Spacer()
+                        TextField("", text: $verify)
+                            .keyboardType(.decimalPad)
+                            .textFieldStyle(.automatic)
+                            .frame(width: 230, height: 40, alignment: .leading)
+                        Spacer()
+                        
+                    }
+                }
+                if(!EnterVerify){
+                    Button(action: {
+                        self.EnterVerify.toggle()
+                    }){
+                        Text("인증번호 전송")
+                            .frame(width: 130, height: 20, alignment: .center)
+                    }
+                    .font(.footnote)
+                    .foregroundColor(.white)
+                    .buttonStyle(.bordered)
                     .background(Color.blue)
                     .cornerRadius(10)
-                    .toolbar{
-                        ToolbarItem(placement: .navigationBarLeading){
-                            Button(action:{
-                            }){
-                                HStack() {
-                                    Image(systemName: "chevron.left")
-                                    Text("메인 페이지")
-                                }
-                            }
-                        }
+                }
+                else{
+                    Button(action: {
+                    }){
+                        Text("인증번호 재전송")
+                            .frame(width: 130, height: 20, alignment: .center)
                     }
-            }.navigationBarTitle("이메일 인증")
+                    .font(.footnote)
+                    .foregroundColor(.white)
+                    .buttonStyle(.bordered)
+                    .background(Color.cyan)
+                    .cornerRadius(10)
+                    Button(action: {
+                    }){
+                        Text("인증번호 입력")
+                            .frame(width: 130, height: 20, alignment: .center)
+                    }
+                    .font(.footnote)
+                    .foregroundColor(.white)
+                    .buttonStyle(.bordered)
+                    .background(Color.blue)
+                    .cornerRadius(10)
+                }
+            }
+            .navigationBarTitle(Text("이메일 인증"))
         }
     }
 }
@@ -36,5 +82,6 @@ struct SignUpView: View {
 struct SignUpView_Previews: PreviewProvider {
     static var previews: some View {
         SignUpView()
+            .previewInterfaceOrientation(.portraitUpsideDown)
     }
 }

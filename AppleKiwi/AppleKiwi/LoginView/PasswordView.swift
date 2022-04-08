fdf27fcf7ca653a445f96471b5aa7ada61b20a7a
MBSoo <qud9783@khu.ac.kr>
2022-04-08 17:59:16 +0900

    Password view

---
+++ AppleKiwi/AppleKiwi/LoginView/PasswordView.swift
@@ -0,0 +1,20 @@
+//
+//  PasswordView.swift
+//  AppleKiwi
+//
+//  Created by MBSoo on 2022/04/08.
+//
+
+import SwiftUI
+
+struct PasswordView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PasswordView_Previews: PreviewProvider {
+    static var previews: some View {
+        PasswordView()
+    }
+}

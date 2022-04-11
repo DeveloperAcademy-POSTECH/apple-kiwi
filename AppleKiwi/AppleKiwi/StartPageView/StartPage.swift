f4cbf8079bd2b09c17098487f2e697473ce97e09
Hyogyong <lhglhs98@gmail.com>
2022-04-11 16:50:15 +0900

    시작페이지 완성 및 팀 로고 에셋에 추가

--- AppleKiwi/AppleKiwi/StartPageView/StartPage.swift
+++ AppleKiwi/AppleKiwi/StartPageView/StartPage.swift
@@ -16,7 +16,7 @@ struct StartPage: View {
     
     var body: some View {
         VStack(){
-            Text("앱카데미 키위").foregroundColor(Color.navKiwi).font(.title)
+            Text("앱카데미 키위").foregroundColor(Color.navKiwi).font(.title .bold())
             Image("normal kiwi").resizable().scaledToFit().frame(width:250)
             Text("시니어, 주니어 러너들에 대한 모든 것").foregroundColor(Color.txtBrown)
             Image("team logo").resizable().scaledToFit().frame(width:100).offset(x:140, y:180)

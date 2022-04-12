fa84bb68a2ea20ae01bca557b3a197443b527097
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2022-04-12 16:43:37 +0900

    마이페이지 비로그인 화면 추가

--- AppleKiwi/AppleKiwi/MyPageView/MyPageView.swift
+++ AppleKiwi/AppleKiwi/MyPageView/MyPageView.swift
@@ -7,69 +7,135 @@
 
 import SwiftUI
 
+
+
+
 struct MyPageView: View {
+    @State private var showSheet = false
+    @State private var isLoggedIn: Bool = true
     var body: some View {
-        NavigationView{
-        VStack() {
-            HStack {
-                Text("마이페이지")
-                    .padding().font(.title)
-                Spacer()
-                Button("로그아웃") {
+        if isLoggedIn{
+            NavigationView{
+                VStack() {
+                    HStack {
+                        Text("마이페이지")
+                            .padding().font(.system(size: 30, weight: .bold))
+                        Spacer()
+                        Button("로그아웃") {
+                            isLoggedIn.toggle()
+                            
+                        }
+                        .foregroundColor(Color.navKiwi)
+                        .padding()
+                        
+                    }.background(.white)
                     
-                }
-                .padding()
-            }.background(.white)
-            
-            HStack{ Image("test")
-                    .resizable()
-                    .edgesIgnoringSafeArea(.all)
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: 100)
-                    .clipShape(Circle()).padding()
-                
-                Text("changBro chaleea22@pos.idserve.net")
-                    .font(.body)
-                    .frame(minWidth: 0, idealWidth: 100, maxWidth: 400, minHeight: 0, maxHeight: 100, alignment: .center)
-                
-                
-                Spacer()
-            }
-            
-            List {
-                NavigationLink(
+                    HStack{ Image("test")
+                            .resizable()
+                            .edgesIgnoringSafeArea(.all)
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 100)
+                            .clipShape(Circle()).padding()
+                        
+                        Text("changBro chaleea22@pos.idserve.net")
+                            .font(.body)
+                            .frame(minWidth: 0, idealWidth: 100, maxWidth: 400, minHeight: 0, maxHeight: 100, alignment: .center)
+                        
+                        
+                        
+                        Spacer()
+                    }
+                    
+                    List {
+                        NavigationLink(
                             destination: NotificationSettingView(),
                             label: {
                                 Text("알림 설정")
                             })
-                NavigationLink(
+                        NavigationLink(
                             destination: ChangePasswordView(),
                             label: {
                                 Text("비밀번호 변경")
                             })
-                    
-                NavigationLink(
+                        
+                        NavigationLink(
                             destination: RuleView(),
                             label: {
                                 Text("기본 방침")
                             })
-              
-                    
-                Button("회원 탈퇴") {
-                    
+                        
+                        
+                        Button("회원 탈퇴") {
+                            
+                        }
+                    }
                 }
+                
             }
+            }
+            
+        
+    else{
+        NavigationView{
+            VStack() {
+                HStack {
+                    Text("마이페이지")
+                        .padding().font(.system(size: 30, weight: .bold))
+                    Spacer()
+                    
+                    NavigationLink(
+                        destination: LoginView(),
+                        label: {
+                            Text("로그인").foregroundColor(Color.navKiwi)
+                        })
+                    .padding()
+                }.background(.white)
+                
+                HStack{ Image("kiwi")
+                        .resizable()
+                        .edgesIgnoringSafeArea(.all)
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 100)
+                        .clipShape(Circle()).padding().offset(x: -80, y:0)
+                    VStack{
+                    Text("로그인이")
+                    .font(.title2).offset(x: -60, y:0)
+                   
+                    
+                    
+                        Text("필요합니다")
+                            .font(.title2)
+                            .offset(x: -50, y:0)
+                    }
+                }
+                
+                List {
+
+                    
+                    NavigationLink(
+                        destination: RuleView(),
+                        label: {
+                            Text("기본 방침")
+                        })
+                    
+                    
+            
+                }
             }
             
         }
         }
-}
-    
+        
+    }
+        
+    }
+
 
 
 struct MyPage_Previews: PreviewProvider {
     static var previews: some View {
         MyPageView()
+            .previewInterfaceOrientation(.portrait)
     }
 }
 

37fb03433dcb968d81faa9713c2e093dfc2f9140
seodam-hst <seodam.hst@gmail.com>
2022-04-13 11:43:38 +0900

    1. 최신 리스트 뷰 추가 2. User.picture이 지정되어 있으면 그 사진을 띄우고, nil이면 잼민 키위를 기본으로 띄우도록 설정함.
    //
    main 브랜치에서 assets - kiwi images 이름이 영어로 바뀌었을 수도 있음.
    메인으로 PR 넣기 전에 main에서 pull 받은 다음에 Users의 sample 데이터에 이미지 문자열 지정해야 함.

--- AppleKiwi/AppleKiwi/MainPageList/MainList.swift
+++ AppleKiwi/AppleKiwi/MainPageList/MainList.swift
@@ -13,82 +13,54 @@ import SwiftUI
 struct MainList : View {
     @State var text : String = ""
     @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
-   // @StateObject var users = AllUsers()
-
+    // @StateObject var users = AllUsers()
+    
     
     @State var users = User.sampleUsers
-
-
+    
+    
     var body :some View{
         
         NavigationView{
             VStack{
-                
-                //검색창에서
+                // 검색창
                 searchBar(text: self.$text)
-                VStack {
-                  //  List {
-                    /*
-                    Section(header: Text("최신리스트")){
+                
+                List {
+                    // 최신 리스트
+                    Section(header: Text("최신 리스트")) {
                         ScrollView(.horizontal) {
                             HStack {
-                                ForEach(users, id: \.id) {user in
-                                    NavigationLink(destination: Text(" \(user.name)")) {
-                                        Circle()
-                                            .fill(Color.blue)
+                                ForEach(users.shuffled().indices) { index in
+                                    NavigationLink(destination: ReadWikiView(user: self.$users[index])) {
+                                        VStack {
+                                        Image(users[index].picture ?? "잼민 키위")
+                                            .resizable()
+                                            .cornerRadius(20)
+                                            .overlay(Circle().stroke(Color("light gray"), lineWidth: 1))
                                             .frame(width: 50, height:50)
+                                        Text(users[index].name)
+                                                .font(.caption2)
+                                                .foregroundColor(Color.black)
+                                        }
                                     }
                                 }
                             }
                         }
                     }
-                    */
-                    List{
-                    ForEach(users.indices)
-                    { index in
-                               NavigationLink(destination: ReadWikiView(user: self.$users[index])){
-                                  Text("\(users[index].name)")
-                               }
-                           }
-                    }
-                    /*
-                        List(users.indices) { index in
-                                   NavigationLink(destination: ReadWikiView(user: self.$users[index])){
-                                      Text("dfds")
-                                   }
-                               }
-                      */
-                        /*
-                        Section(header: Text("전체리스트"))
-                        {
-                            ForEach(users.filter({"\($0)".contains(self.text) || self.text.isEmpty}), id: \.id) {  user in
-                                NavigationLink( destination: ReadWikiView(users: user))
-                                {
-                                    Text("(user.name)")
-                                }
+                    
+                    // 전체 리스트
+                    Section(header: Text("전체 리스트")) {
+                        ForEach(users.indices)
+                        { index in
+                            NavigationLink(destination: ReadWikiView(user: self.$users[index])) {
+                                Text("\(users[index].name)")
                             }
                         }
-                        */
-                        
-                                      /*  Section(header: Text("전체리스트"))
-                                        {
-                                            ForEach(Array(users.Users.enumerated().filter({"\($0)".contains(self.text) || self.text.isEmpty})), id: \.1) { i, user in
-                                                NavigationLink( destination: ReadWikiView(user:  $users.Users[i]))
-                                                {
-                                                    Text("\(user.name)")
-                                                }
-                                            }
-                                        }*/
-                                   // }.listStyle(.grouped)
-                             
-                                
+                    }
                 }
-                            
-              
-                
             }.navigationBarHidden(true)
-       }
-      
+        }
     }
 }
 
@@ -97,3 +69,4 @@ struct MainList_Previews: PreviewProvider {
         MainList()
     }
 }
+

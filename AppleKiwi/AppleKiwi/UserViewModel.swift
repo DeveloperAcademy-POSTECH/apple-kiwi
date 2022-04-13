070008f52b0691a1218749d7cba5fd5ba576ba4e
Kyubo Shim <anti9ua@Kyuboui-MacBookPro.local>
2022-04-13 14:40:38 +0900

    asdf

--- AppleKiwi/AppleKiwi/UserViewModel.swift
+++ AppleKiwi/AppleKiwi/UserViewModel.swift
@@ -10,6 +10,7 @@ import Foundation
 struct UserViewModel {
     var email: String = ""
     var password: String = ""
+    var confirmPassword: String = ""
     var nickname: String = ""
     var name: String = ""
     var ID: String = ""
@@ -36,18 +37,75 @@ struct UserViewModel {
     /* email valid -> true */
     /* email not valid -> false */
     /*############################*/
-    var validEmailAddress:Bool {
-        if isEmailValid(_email: email) {
-            return false
-        }
-        else {
-            return true
-        }
-    }
     
     func isEmailValid(_email: String) -> Bool {
         let emailRegex = "[A-Z0-9a-z._%+-]+@pos.idserve.net"
         let emailTest = NSPredicate(format: "SELF MATCHES %@", emailRegex)
         return emailTest.evaluate(with: email)
+    }
+    
+    func passwordMatch(_confirmPw: String) -> Bool {
+        return _confirmPw == password
+    }
+    
+    var isSignInComplete: Bool {
+        if !isEmailValid(_email: email) ||
+            isEmpty(_field: nickname) ||
+            !passwordMatch(_confirmPw: confirmPassword) {
+            return false
+        }
+        return true
+    }
+    
+    var isLoginComplete: Bool {
+        if isEmpty(_field: email) || isEmpty(_field: password) {
+            return false
+        }
+        return true
+    }
+    
+    var validEmailAddress: String {
+        if isEmailValid(_email: email) {
+            return ""
+        }
+        else {
+            return "올바른 이메일 주소를 입력해주세요."
+        }
+    }
+    
+    var isNameValid: String {
+        if !isEmpty(_field: name) {
+            return ""
+        }
+        else {
+            return "이름을 입력해주세요."
+        }
+    }
+    
+    var isNicknameValid: String {
+        if !isEmpty(_field: nickname) {
+            return ""
+        }
+        else {
+            return "닉네임을 입력해주세요."
+        }
+    }
+    
+    var isPasswordValid: String {
+        if !isEmpty(_field: password) {
+            return ""
+        }
+        else {
+            return "비밀번호를 입력해주세요!"
+        }
+    }
+    
+    var isConfirmPasswordValid: String {
+        if passwordMatch(_confirmPw: confirmPassword) {
+            return ""
+        }
+        else {
+            return "동일한 비밀번호를 입력해주세요."
+        }
     }
 }

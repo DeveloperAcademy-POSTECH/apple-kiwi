0253e982713855e7be30e7380390965149429e84
Kyubo Shim <anti9ua@Kyuboui-MacBookPro.local>
2022-04-14 22:48:05 +0900

    markdown edit

--- AppleKiwi/AppleKiwi/LoginView/SignInView.swift
+++ AppleKiwi/AppleKiwi/LoginView/SignInView.swift
@@ -9,8 +9,8 @@ import SwiftUI
 
 let lightGray = Color(red: 239.0/255.0, green: 243.0/255.0, blue: 244.0/255.0)
 
-let storedEmail = "admin"
-let storedPassword = "admin"
+let storedEmail = "sunim22"
+let storedPassword = "sunim22"
 
 struct SignInRootKey: EnvironmentKey {
     static let defaultValue: [Binding<Bool>] = []
